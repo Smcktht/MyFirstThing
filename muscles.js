2d2bd080ab5240075a421da58d821bb51e5d57a1
Smcktht <marahsckee@yahoo.com>
2024-03-19 17:52:50 -0400

    that new new

---
+++ muscles.js
@@ -0,0 +1,3 @@
+document.getElementById ("CCweb").onclick=function(){
+     location.href="https://www.coffeecoff.ee/"
+    } 
---
+++ shits and gigs/anotherone.js
@@ -0,0 +1,3 @@
+document.getElementById ("ricky").onclick=function(){
+    location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+}
